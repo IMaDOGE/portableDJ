4c3bfeacba618e4750ea726e6fbcab070084e565
Zoltán Tóth <zolcsika2001@gmail.com>
2023-11-27 13:46:24 +0100

    init

---
+++ app/src/main/java/hu/unideb/inf/mobil2023/projekt2/MainActivity.java
@@ -0,0 +1,14 @@
+package hu.unideb.inf.mobil2023.projekt2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
