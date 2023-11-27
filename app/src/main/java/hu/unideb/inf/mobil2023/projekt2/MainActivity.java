e298ec808acdec0aff1b395faffa2715ec1b000a
Zoltán Tóth <zolcsika2001@gmail.com>
2023-11-27 14:15:27 +0100

    test ui for concept demo

--- app/src/main/java/hu/unideb/inf/mobil2023/projekt2/MainActivity.java
+++ app/src/main/java/hu/unideb/inf/mobil2023/projekt2/MainActivity.java
@@ -2,13 +2,36 @@ package hu.unideb.inf.mobil2023.projekt2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE); // force landscape mode
+
         setContentView(R.layout.activity_main);
+    }
+
+    public void leftPlayPressed(View view) {
+    }
+
+    public void leftPausePressed(View view) {
+    }
+
+    public void leftStopPressed(View view) {
+    }
+
+    public void rightPlayPressed(View view) {
+    }
+
+    public void rightPausePressed(View view) {
+    }
+
+    public void rightStopPressed(View view) {
     }
 }
